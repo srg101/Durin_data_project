bf230069e32fd768e49b4e77059676d0fe941e01
Hilary Rose Dawson <hrosedawson@gmail.com>
2023-08-29 16:37:29 -0700

    Fine-tuned initial code

--- R code/leaf scan data.R
+++ R code/leaf scan data.R
@@ -7,21 +7,23 @@ library(dataDownloader)
 # This function doesn't work while DURIN is not public
 # Download manually for now
 get_file(node = "f4v9t",
-         file = "PFTC6_microclimate_2022.zip",
+         file = "DURIN_2023_Leaf_Areas.zip",
          path = "raw_data",
          remote_path = "Vegetation/raw_data/Leaf scans")
 
 #Unzip microclimate data
-unzip("raw_data/PFTC6_microclimate_2022.zip", exdir = "raw_data/leaf_scans")
-file.remove("raw_data/PFTC6_microclimate_2022.zip") #let's free some space
+unzip("raw_data/DURIN_2023_Leaf_Areas.zip", exdir = "raw_data/leaf_scans")
+file.remove("raw_data/DURIN_2023_Leaf_Areas.zip") #let's free some space
 
 ## Read in files ----
 # Make file list
-filesPFTC6 <- dir(path = "raw_data/leaf_scans", pattern = "^data.*\\.csv$", full.names = TRUE, recursive = TRUE)
+filesDURIN <- dir(path = "raw_data/leaf_scans/DURIN_2023_Leaf_Areas", pattern = ".csv", full.names = TRUE, recursive = TRUE)
 
 # Read in data
-tempPFTC6 <- map_df(set_names(files), function(file) {
+tempDURIN <- map_df(set_names(filesDURIN), function(file) {
   file %>%
     set_names() %>%
-    map_df(~ read_csv2(file = file, col_names = FALSE)) #important! read_csv2 reads in European format
+    map_df(~ read.csv(file = file)) #important! reading in American format
 }, .id = "File")
+
+str(tempDURIN)
